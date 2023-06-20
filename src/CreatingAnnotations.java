2fb02082bcd178fc475df6e44b76cce31256d797
boogramer <dvelaflores84@gmail.com>
2023-06-19 21:53:44 -0500

    Ahora intentamos implementar metodo printAction

--- src/CreatingAnnotations.java
+++ src/CreatingAnnotations.java
@@ -1,25 +1,26 @@
-import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-
 public class CreatingAnnotations {
-    public static <T> boolean validador(T objeto){
+    public static boolean validador(Object objeto){
         if (objeto.getClass().isAnnotationPresent(VeryImportant.class)) {
             System.out.println("This class is very important");
         } else {
             System.out.println("This class is NOT very important");
         }
-        return false;
+        return true;
     }
-/*
-    for (Method method : myPussy.getClass().getDeclaredMethods()) {
-        if (method.isAnnotationPresent(RunImmediately.class)) {
-            RunImmediately annotation = method.getAnnotation(RunImmediately.class);
-            for (int i = 0; i < annotation.times(); i++) {
-                method.invoke(myPussy);
+
+    public static boolean printActiob(Object objeto){
+        for (Method method : myPussy.getClass().getDeclaredMethods()) {
+            if (method.isAnnotationPresent(RunImmediately.class)) {
+                RunImmediately annotation = method.getAnnotation(RunImmediately.class);
+                for (int i = 0; i < annotation.times(); i++) {
+                    method.invoke(myPussy);
+                }
             }
         }
+        return false;
     }
+
+  /*
     for (Field field : myPussy.getClass().getDeclaredFields()) {
         if (field.isAnnotationPresent(ImportantString.class)) {
             Object objectValue = field.get(myPussy);
--- src/TestPuppies.java
+++ src/TestPuppies.java
@@ -2,6 +2,6 @@ public class TestPuppies {
     public static void main(String[] args) {
         Cat myCat = new Cat("July");
         Puss myPussy = new Puss("Patito Onicha");
-        System.out.print(CreatingAnnotations.validador(myPussy));
+        System.out.println(CreatingAnnotations.validador(myPussy));
     }
 }
