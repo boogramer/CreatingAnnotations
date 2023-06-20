99add445133467e9079821b43528c8fe023f09fc
boogramer <dvelaflores84@gmail.com>
2023-06-19 21:57:54 -0500

    Implementando nameToUpperCase method

--- src/CreatingAnnotations.java
+++ src/CreatingAnnotations.java
@@ -1,3 +1,7 @@
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 public class CreatingAnnotations {
     public static boolean validador(Object objeto){
         if (objeto.getClass().isAnnotationPresent(VeryImportant.class)) {
@@ -5,28 +9,30 @@ public class CreatingAnnotations {
         } else {
             System.out.println("This class is NOT very important");
         }
-        return true;
+        return false;
     }
 
-    public static boolean printActiob(Object objeto){
-        for (Method method : myPussy.getClass().getDeclaredMethods()) {
+    public static boolean printActiob(Object objeto) throws InvocationTargetException, IllegalAccessException {
+        for (Method method : objeto.getClass().getDeclaredMethods()) {
             if (method.isAnnotationPresent(RunImmediately.class)) {
                 RunImmediately annotation = method.getAnnotation(RunImmediately.class);
                 for (int i = 0; i < annotation.times(); i++) {
-                    method.invoke(myPussy);
+                    method.invoke(objeto);
                 }
             }
         }
         return false;
     }
 
-  /*
-    for (Field field : myPussy.getClass().getDeclaredFields()) {
-        if (field.isAnnotationPresent(ImportantString.class)) {
-            Object objectValue = field.get(myPussy);
-            if (objectValue instanceof  String stringValue) {
-                System.out.println(stringValue.toUpperCase());
-            }
-        }
-    }*/
+  public static boolean nameToUpperCase(Object objeto){
+      for (Field field : objeto.getClass().getDeclaredFields()) {
+          if (field.isAnnotationPresent(ImportantString.class)) {
+              Object objectValue = field.get(objeto);
+              if (objectValue instanceof  String stringValue) {
+                  System.out.println(stringValue.toUpperCase());
+              }
+          }
+      }
+      return false;
+  }
 }
--- src/TestPuppies.java
+++ src/TestPuppies.java
@@ -1,7 +1,10 @@
+import java.lang.reflect.InvocationTargetException;
+
 public class TestPuppies {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InvocationTargetException, IllegalAccessException {
         Cat myCat = new Cat("July");
         Puss myPussy = new Puss("Patito Onicha");
-        System.out.println(CreatingAnnotations.validador(myPussy));
+        //System.out.println(CreatingAnnotations.validador(myPussy));
+        System.out.println(CreatingAnnotations.printActiob(myPussy));
     }
 }
