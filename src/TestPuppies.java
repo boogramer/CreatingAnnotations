b5ee9e867a7e59d5c38e778aaf8c70c108ee7cba
boogramer <dvelaflores84@gmail.com>
2023-06-19 21:29:56 -0500

    Logre hacer funcionar Testpuppies pero no se como hacer para que el metodo no sea booleano

--- src/TestPuppies.java
+++ src/TestPuppies.java
@@ -1,2 +1,7 @@
 public class TestPuppies {
+    public static void main(String[] args) {
+        Cat myCat = new Cat("July");
+        Puss myPussy = new Puss("Patito Onicha");
+        System.out.print(CreatingAnnotations.validador(myPussy));
+    }
 }
